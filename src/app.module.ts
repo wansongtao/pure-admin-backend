e30e5fb2dc07adad0e09407a4597c699e1b81086
wansongtao <18101837209@163.com>
2024-05-28 20:51:39 +0800

    feat: 注册PrismaModule为全局模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true,
       cache: true,
+    }),
+    PrismaModule.forRootAsync({
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => {
+        return {
+          prismaOptions: {
+            datasources: {
+              db: {
+                url: configService.get<string>('DATABASE_URL'),
+              },
+            },
+          },
+          explicitConnect: false,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
 })
