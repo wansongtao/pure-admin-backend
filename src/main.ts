4c74da527007e2a5d59dccf10bff94f1ed3fcfed
wansongtao <18101837209@163.com>
2024-05-28 21:10:34 +0800

    feat: 设置swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT');
+  const projectConfig = app.get(ConfigService);
 
+  const prefix = projectConfig.get<string>('PREFIX');
+  app.setGlobalPrefix(prefix);
+
+  const title = projectConfig.get<string>('SWAGGER_TITLE');
+  const description = projectConfig.get<string>('SWAGGER_DESCRIPTION');
+  const version = projectConfig.get<string>('SWAGGER_VERSION');
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(title)
+    .setDescription(description)
+    .setVersion(version)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  const port = projectConfig.get<number>('PORT');
   await app.listen(port);
 }
 bootstrap();
