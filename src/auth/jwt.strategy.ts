038aa9f7442b904caa44ceb1bbf16de36be5f63a
wansongtao <18101837209@163.com>
2024-07-07 22:41:27 +0800

    feat: jwt策略

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    const secret = configService.get('JWT_SECRET');
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.userId, username: payload.username };
+  }
+}
