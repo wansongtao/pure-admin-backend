b722b6243e2df94aa60851b769acc15ac6b89f3c
wansongtao <18101837209@163.com>
2024-07-08 15:06:06 +0800

    feat: 添加登录校验错误信息

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,20 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, MinLength, Matches } from 'class-validator';
+import { IsNotEmpty, Matches } from 'class-validator';
 
 export class LoginDto {
   @Matches(/^[a-zA-Z][a-zA-Z0-9]{4,10}$/, { message: '用户名格式错误' })
+  @IsNotEmpty({ message: '用户名不能为空' })
   @ApiProperty()
   userName: string;
 
   @Matches(/^[a-zA-Z](?=.*[.?!&_])(?=.*\d)[a-zA-Z\d.?!&_]{5,15}$/, {
     message: '密码格式错误',
   })
+  @IsNotEmpty({ message: '密码不能为空' })
   @ApiProperty()
   password: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(4)
-  @ApiProperty()
+  @Matches(/^[a-zA-Z0-9]{4}$/, { message: '验证码格式错误' })
+  @IsNotEmpty({ message: '验证码不能为空' })
+  @ApiProperty({ description: '验证码' })
   captcha: string;
 }
