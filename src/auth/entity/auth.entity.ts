56fabae6f3fdc8f487e7680049c995262ba32468
wansongtao <18101837209@163.com>
2024-07-08 16:23:01 +0800

    feat: 新增登录接口返回实体

--- src/auth/entity/auth.entity.ts
+++ src/auth/entity/auth.entity.ts
@@ -1,3 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class AuthEntity {
-  data: string;
+  @ApiProperty({
+    description: 'base64 格式验证码',
+    default: 'data:image/svg+xml;base64,***',
+  })
+  captcha: string;
+}
+
+export class LoginEntity {
+  @ApiProperty({ description: 'token' })
+  token: string;
 }
