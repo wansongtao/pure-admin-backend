9eea09e9f3f119270a2daa89c5d1335868abb7dc
wansongtao <18101837209@163.com>
2024-07-09 15:23:13 +0800

    feat: 添加接口认证标识

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,10 +10,11 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('users')
 @ApiTags('users')
+@ApiBearerAuth()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
