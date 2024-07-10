20a5c626afa96a841cf7e2dfacc905d463fccf10
wansongtao <18101837209@163.com>
2024-07-10 15:38:27 +0800

    style: 代码格式优化

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,33 +1,32 @@
 import { Controller, Get, Ip, Headers, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { LoginDto } from './dto/login.dto';
 import { AuthEntity, LoginEntity } from './entities/auth.entity';
 import { ApiBaseResponse } from 'src/common/decorators/api-response.decorator';
 import { Public } from 'src/common/decorators/public.decorator';
 
-@Controller('auth')
 @ApiTags('auth')
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Public()
-  @Get('captcha')
   @ApiOperation({
     summary: '获取验证码',
   })
   @ApiBaseResponse(AuthEntity)
+  @Public()
+  @Get('captcha')
   getCaptcha(@Ip() ip: string, @Headers('user-agent') userAgent: string) {
     return this.authService.generateCaptcha(ip, userAgent);
   }
 
-  @Public()
-  @Post('login')
   @ApiOperation({
     summary: '用户登录',
   })
-  @ApiBody({ type: LoginDto })
   @ApiBaseResponse(LoginEntity)
+  @Public()
+  @Post('login')
   login(
     @Body() { userName, password, captcha }: LoginDto,
     @Ip() ip: string,
@@ -39,11 +38,11 @@ export class AuthController {
     });
   }
 
-  @Get('logout')
   @ApiOperation({
     summary: '用户登出',
   })
   @ApiBaseResponse()
+  @Get('logout')
   logout(@Headers('authorization') token: string) {
     return this.authService.logout(token.split(' ')[1]);
   }
