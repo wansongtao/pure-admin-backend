1d30a910c6df9c7f79f6141b0d3c4d00a0b51924
wansongtao <18101837209@163.com>
2024-07-10 15:41:50 +0800

    feat: 暂停创建/编辑用户

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'nestjs-prisma';
-import { hash } from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+// import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -13,13 +13,28 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    const hashedPassword = await hash(
-      createUserDto.password,
-      +this.config.get('BCRYPT_SALT_ROUNDS'),
-    );
-    createUserDto.password = hashedPassword;
+    console.log(createUserDto);
+    return null;
 
-    return this.prisma.user.create({ data: createUserDto });
+    // const password = this.config.get('DEFAULT_PASSWORD') || 'd.123456';
+    // const hashedPassword = await hash(
+    //   password,
+    //   +this.config.get('BCRYPT_SALT_ROUNDS') || 10,
+    // );
+
+    // return this.prisma.user.create({
+    //   data: {
+    //     userName: createUserDto.userName,
+    //     password: hashedPassword,
+    //     disabled: createUserDto.disabled,
+    //     profile: {
+    //       create: {
+    //         nickName: createUserDto.nickName,
+    //         avatar: createUserDto.avatar,
+    //       },
+    //     },
+    //   },
+    // });
   }
 
   findAll() {
@@ -48,14 +63,17 @@ export class UsersService {
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
-    if (updateUserDto.password) {
-      updateUserDto.password = await hash(
-        updateUserDto.password,
-        +this.config.get<string>('BCRYPT_SALT_ROUNDS'),
-      );
-    }
+    console.log(updateUserDto);
 
-    return this.prisma.user.update({ where: { id }, data: updateUserDto });
+    return null;
+    // if (updateUserDto.password) {
+    //   updateUserDto.password = await hash(
+    //     updateUserDto.password,
+    //     +this.config.get<string>('BCRYPT_SALT_ROUNDS'),
+    //   );
+    // }
+
+    // return this.prisma.user.update({ where: { id }, data: updateUserDto });
   }
 
   remove(id: string) {
