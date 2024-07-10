81904b91f873031bf7114a9ab2e822adb13d5337
wansongtao <18101837209@163.com>
2024-07-10 15:41:01 +0800

    feat: 创建与编辑用户的参数dto配置更改

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,15 +1,34 @@
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  Matches,
+  IsBoolean,
+  IsEmpty,
+  MaxLength,
+} from 'class-validator';
 
 export class CreateUserDto {
+  @Matches(/^[a-zA-Z][a-zA-Z0-9]{4,10}$/, { message: '用户名格式错误' })
+  @IsNotEmpty({ message: '用户名不能为空' })
   @ApiProperty()
   userName: string;
 
-  @ApiProperty()
-  password: string;
+  @Matches(/^[a-zA-Z\u4e00-\u9fa5']{1,50}$/, { message: '昵称格式错误' })
+  @IsEmpty()
+  @ApiProperty({ required: false, default: '' })
+  nickName?: string;
 
-  @ApiProperty({ required: false })
+  @IsBoolean({ message: '禁用状态必须为布尔值' })
+  @IsEmpty()
+  @ApiProperty({ required: false, default: false })
   disabled?: boolean;
 
-  @ApiProperty({ required: false })
-  deleted?: boolean;
+  @MaxLength(255, { message: '头像url长度不能超过255' })
+  @IsEmpty()
+  @ApiProperty({ required: false, default: '' })
+  avatar?: string;
+
+  @IsEmpty()
+  @ApiProperty({ required: false, description: '角色id列表', default: null })
+  roles?: string[];
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, PickType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(
+  PickType(CreateUserDto, ['avatar', 'disabled', 'nickName', 'roles']),
+) {}
