2218aef476d7ea768551d006f06d4ebd0c071e32
wansongtao <18101837209@163.com>
2024-07-11 21:14:04 +0800

    feat: 处理用户登录后，用户被删除的情况

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,11 +1,15 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
     const secret = configService.get('JWT_SECRET');
 
     super({
@@ -15,7 +19,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { userId: payload.userId, username: payload.username };
+  async validate(payload: { userId: string; userName: string }) {
+    const user = await this.usersService.validateUser(payload.userName);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return { userId: payload.userId, userName: payload.userName };
   }
 }
