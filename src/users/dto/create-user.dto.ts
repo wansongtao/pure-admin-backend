baee1c5db0811543f45fbfc1f52fd7ed4074aad3
wansongtao <18101837209@163.com>
2024-07-12 11:23:38 +0800

    perf: 创建用户dto优化

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -4,9 +4,9 @@ import {
   Matches,
   IsBoolean,
   MaxLength,
-  ValidateIf,
   IsNumber,
   IsUrl,
+  IsOptional,
 } from 'class-validator';
 
 export class CreateUserDto {
@@ -15,24 +15,24 @@ export class CreateUserDto {
   @ApiProperty()
   userName: string;
 
-  @Matches(/^[a-zA-Z\u4e00-\u9fa5']{1,50}$/, { message: '昵称格式错误' })
-  @ValidateIf((o) => o.nickName !== undefined)
+  @Matches(/^[a-zA-Z0-9\u4e00-\u9fa5']{1,50}$/, { message: '昵称格式错误' })
+  @IsOptional()
   @ApiProperty({ required: false, default: '' })
   nickName?: string;
 
   @IsBoolean({ message: '禁用状态必须为布尔值' })
-  @ValidateIf((o) => o.disabled !== undefined)
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   disabled?: boolean;
 
   @MaxLength(255, { message: '头像url长度不能超过255' })
   @IsUrl(undefined, { message: '头像url格式错误' })
-  @ValidateIf((o) => o.avatar !== undefined)
+  @IsOptional()
   @ApiProperty({ required: false, default: '' })
   avatar?: string;
 
   @IsNumber({}, { message: '角色id列表必须为数组', each: true })
-  @ValidateIf((o) => o.roles !== undefined)
+  @IsOptional()
   @ApiProperty({ required: false, description: '角色id列表', default: null })
   roles?: number[];
 }
