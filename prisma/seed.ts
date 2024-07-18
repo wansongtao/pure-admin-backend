1ed089703af02ee53b1901866db600d9dbef9b83
wansongtao <18101837209@163.com>
2024-07-18 10:23:21 +0800

    refactor: 重构权限表结构

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,116 +4,6 @@ import { hash } from 'bcrypt';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.permission.createMany({
-    data: [
-      {
-        id: 1,
-        name: '系统管理',
-        path: 'system',
-        type: 'DIRECTORY',
-        icon: 'system',
-      },
-      {
-        id: 2,
-        name: '用户管理',
-        pid: 1,
-        path: 'user',
-        icon: 'user',
-        component: '/system/user/index.vue',
-        cache: true,
-      },
-      {
-        id: 3,
-        name: '添加用户',
-        pid: 2,
-        type: 'BUTTON',
-        permission: 'system:user:add',
-      },
-      {
-        id: 4,
-        name: '编辑用户',
-        pid: 2,
-        type: 'BUTTON',
-        permission: 'system:user:edit',
-      },
-      {
-        id: 5,
-        name: '删除用户',
-        pid: 2,
-        type: 'BUTTON',
-        permission: 'system:user:del',
-      },
-      {
-        id: 6,
-        name: '导出用户',
-        pid: 2,
-        type: 'BUTTON',
-        permission: 'system:user:export',
-      },
-      {
-        id: 7,
-        name: '菜单管理',
-        pid: 1,
-        path: 'menu',
-        icon: 'menu',
-        component: '/system/menu/index.vue',
-        cache: true,
-      },
-      {
-        id: 8,
-        name: '添加菜单',
-        pid: 7,
-        type: 'BUTTON',
-        permission: 'system:menu:add',
-      },
-      {
-        id: 9,
-        name: '编辑菜单',
-        pid: 7,
-        type: 'BUTTON',
-        permission: 'system:menu:edit',
-      },
-      {
-        id: 10,
-        name: '删除菜单',
-        pid: 7,
-        type: 'BUTTON',
-        permission: 'system:menu:del',
-      },
-      {
-        id: 11,
-        name: '角色管理',
-        pid: 1,
-        path: 'role',
-        icon: 'role',
-        component: '/system/role/index.vue',
-        cache: true,
-      },
-      {
-        id: 12,
-        name: '添加角色',
-        pid: 11,
-        type: 'BUTTON',
-        permission: 'system:role:add',
-      },
-      {
-        id: 13,
-        name: '编辑角色',
-        pid: 11,
-        type: 'BUTTON',
-        permission: 'system:role:edit',
-      },
-      {
-        id: 14,
-        name: '删除角色',
-        pid: 11,
-        type: 'BUTTON',
-        permission: 'system:role:del',
-      },
-    ],
-    skipDuplicates: true,
-  });
-
   const role = await prisma.role.create({
     data: {
       name: 'sAdmin',
@@ -121,65 +11,170 @@ async function main() {
     },
   });
 
-  await prisma.roleInPermission.createMany({
-    data: [
-      {
-        roleId: role.id,
-        permissionId: 1,
+  await prisma.permission.create({
+    data: {
+      name: '系统管理',
+      path: 'system',
+      type: 'DIRECTORY',
+      icon: 'system',
+      roleInPermission: {
+        create: {
+          roleId: role.id,
+        },
       },
-      {
-        roleId: role.id,
-        permissionId: 2,
+      children: {
+        create: [
+          {
+            name: '用户管理',
+            path: 'user',
+            icon: 'user',
+            component: '/system/user/index.vue',
+            cache: true,
+            roleInPermission: {
+              create: {
+                roleId: role.id,
+              },
+            },
+            children: {
+              create: [
+                {
+                  name: '添加用户',
+                  type: 'BUTTON',
+                  permission: 'system:user:add',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+                {
+                  name: '编辑用户',
+                  type: 'BUTTON',
+                  permission: 'system:user:edit',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+                {
+                  name: '删除用户',
+                  type: 'BUTTON',
+                  permission: 'system:user:del',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+                {
+                  name: '导出用户',
+                  type: 'BUTTON',
+                  permission: 'system:user:export',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+          {
+            name: '菜单管理',
+            path: 'menu',
+            icon: 'menu',
+            component: '/system/menu/index.vue',
+            cache: true,
+            roleInPermission: {
+              create: {
+                roleId: role.id,
+              },
+            },
+            children: {
+              create: [
+                {
+                  name: '添加菜单',
+                  type: 'BUTTON',
+                  permission: 'system:menu:add',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+                {
+                  name: '编辑菜单',
+                  type: 'BUTTON',
+                  permission: 'system:menu:edit',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+                {
+                  name: '删除菜单',
+                  type: 'BUTTON',
+                  permission: 'system:menu:del',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+          {
+            name: '角色管理',
+            path: 'role',
+            icon: 'role',
+            component: '/system/role/index.vue',
+            cache: true,
+            roleInPermission: {
+              create: {
+                roleId: role.id,
+              },
+            },
+            children: {
+              create: [
+                {
+                  name: '添加角色',
+                  type: 'BUTTON',
+                  permission: 'system:role:add',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+                {
+                  name: '编辑角色',
+                  type: 'BUTTON',
+                  permission: 'system:role:edit',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+                {
+                  name: '删除角色',
+                  type: 'BUTTON',
+                  permission: 'system:role:del',
+                  roleInPermission: {
+                    create: {
+                      roleId: role.id,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        ],
       },
-      {
-        roleId: role.id,
-        permissionId: 3,
-      },
-      {
-        roleId: role.id,
-        permissionId: 4,
-      },
-      {
-        roleId: role.id,
-        permissionId: 5,
-      },
-      {
-        roleId: role.id,
-        permissionId: 6,
-      },
-      {
-        roleId: role.id,
-        permissionId: 7,
-      },
-      {
-        roleId: role.id,
-        permissionId: 8,
-      },
-      {
-        roleId: role.id,
-        permissionId: 9,
-      },
-      {
-        roleId: role.id,
-        permissionId: 10,
-      },
-      {
-        roleId: role.id,
-        permissionId: 11,
-      },
-      {
-        roleId: role.id,
-        permissionId: 12,
-      },
-      {
-        roleId: role.id,
-        permissionId: 13,
-      },
-      {
-        roleId: role.id,
-        permissionId: 14,
-      },
-    ],
+    },
   });
 
   const password = await hash('w.1admin', +process.env.BCRYPT_SALT_ROUNDS);
