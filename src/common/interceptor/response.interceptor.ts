d620caa585b3f6c8a8486f37d23cde4cb9d6c9e7
wansongtao <18101837209@163.com>
2024-08-10 10:19:40 +0800

    perf: 优化拦截器，抛出错误给过滤器

--- src/common/interceptor/response.interceptor.ts
+++ src/common/interceptor/response.interceptor.ts
@@ -5,17 +5,19 @@ import {
   CallHandler,
   StreamableFile,
 } from '@nestjs/common';
-import { map } from 'rxjs/operators';
 import { BaseResponseEntity } from '../entities/api-response.entity';
+import { throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable()
-export class ResponseInterceptor<T>
-  implements
-    NestInterceptor<T, BaseResponseEntity<T> | StreamableFile | Buffer>
-{
+export class ResponseInterceptor<T> implements NestInterceptor<T, any> {
   intercept(context: ExecutionContext, next: CallHandler) {
     return next.handle().pipe(
       map((data) => {
+        if (data instanceof Error) {
+          throw data;
+        }
+
         if (data instanceof StreamableFile || Buffer.isBuffer(data)) {
           return data;
         }
@@ -33,6 +35,9 @@ export class ResponseInterceptor<T>
         }
 
         return result;
+      }),
+      catchError((err) => {
+        return throwError(() => err);
       }),
     );
   }
